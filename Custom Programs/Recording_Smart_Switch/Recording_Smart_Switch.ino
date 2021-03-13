4cea97a424bbc2cf3fb5f2de23fde19401e1d0fa
Richard Garza <rgactr@gmail.com>
2021-03-13 15:55:50 -0800

    Implemented logic for buttons on Recording Switch

--- Custom Programs/Recording_Smart_Switch/Recording_Smart_Switch.ino
+++ Custom Programs/Recording_Smart_Switch/Recording_Smart_Switch.ino
@@ -10,8 +10,10 @@
 #define LED_SCREEN 7    
 
 
-// Then I initialize the button states as HIGH since I'm using the 
-// internal pullup resistors and I initialize the relay states as LOW.
+
+// Then I define the initial button states as HIGH since I'm using the 
+// internal pullup resistors and I initialize the relay states as LOW 
+// so they are off when the program starts.
 
 int OFF_btn_state = HIGH;         
 int ON_btn_state = HIGH;         
@@ -20,38 +22,92 @@ int REC_btn_state = HIGH;
 int RLY_ONE_state = LOW;
 int RLY_TWO_state = LOW;
 
+
+
 // Now I'll initialize the global state as OFF. 
 // Since I'll have 3 global states, I'm using 1, 2, and 3 for simplicity. 
 // 1 = OFF, 2 = ON, 3 = REC
 
 int GLOBAL_state = 1;
 
-void setup() {
-
-  Serial.begin(9600);
-
-// Set the pinMode for both active pins
-  pinMode(button, INPUT_PULLUP);
-  pinMode(3,OUTPUT);
-  pinMode(LED,OUTPUT);    //Sets the pinMode to Output 
+// and I'll define the set_global_state function
+int Set_global_state (int x){
+  GLOBAL_state = x;
 }
 
 
+
+// Now I'll define the setup function
+void setup() {
+
+// First I'll initialize my serial connection
+// (The serial connection is temporary for programming/debugging) 
+
+// Init serial connection
+  Serial.begin(9600);
+
+// Then Set the pinMode for inputs
+  pinMode(OFF_btn, INPUT_PULLUP);
+  pinMode(ON_btn, INPUT_PULLUP);
+  pinMode(REC_btn, INPUT_PULLUP);  
+
+// Then Set the pinMode for outputs
+  pinMode(RLY_ONE,OUTPUT);
+  pinMode(RLY_TWO,OUTPUT);
+  pinMode(LED_SCREEN,OUTPUT);
+}
+
+
+
+// Now I'll define the loop function
 void loop() {
+
+// First I read the state of each button
+  OFF_btn_state = digitalRead(OFF_btn);         
+  ON_btn_state = digitalRead(ON_btn);         
+  REC_btn_state = digitalRead(REC_btn);
+
+// Then I'll compare the following: 
+// Is a button being pressed? 
+// Is that button the only one being pressed? 
+// Is the desired state already active? 
+
+// Off button pressed
+if(
+  OFF_btn_state == LOW &&
+  ON_btn_state == HIGH &&
+  REC_btn_state == HIGH  &&
+  GLOBAL_state != 1
+  ){
+  Set_global_state (1); 
+} 
+
+// On button pressed
+else if (
+  OFF_btn_state == HIGH &&
+  ON_btn_state == LOW &&
+  REC_btn_state == HIGH  &&
+  GLOBAL_state != 2
+  ){
+  Set_global_state (2); 
+} 
+
+// Record button pressed
+else if (
+  OFF_btn_state == HIGH &&
+  ON_btn_state == HIGH &&
+  REC_btn_state == LOW  &&
+  GLOBAL_state != 3
+  ){
+  Set_global_state (3);
+}
+
 
 // Set button_state based on state of button
   button_state = digitalRead(button);
 
   Serial.println(button_state);
   delay(1000);
-
-  if(pin3OUTPUT == 0){
-    digitalWrite(3, 1);
-    pin3OUTPUT = 1;
-  } else {
-    digitalWrite(3,0);
-    pin3OUTPUT = 0;
-  }
 
   
 // if button is pressed, turn on LED
